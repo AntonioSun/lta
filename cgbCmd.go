1f160a8588c38f02c961d1421f075413e664c6c5
AntonioSun <AntonioSun@users.noreply.github.com>
2017-12-16 10:44:48 -0500

    Update cgbCmd.go

--- cgbCmd.go
+++ cgbCmd.go
@@ -13,7 +13,7 @@ func cgbCmd(options Options) error {
 	fmt.Printf("Instance Listing\r\n")
 	for _, pods := range config.Pod {
 		for _, inst := range pods.Instance {
-			fmt.Printf("\r\n%s:\r\n  https://perftest%s.dayforce.com/\r\n",
+			fmt.Printf("\r\n%s:\r\n  https://perftest%s.myhost.com/\r\n",
 				pods.Id, pods.Id)
 			fmt.Printf("  Client/DB Name: %s\r\n", inst.Database)
 			fmt.Printf("  DB Server: %s\r\n", inst.Dbserver)
